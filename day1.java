5620db391428101bec74599bdb94009a8e4074b9
Suryakant <surajdwivedi644@gmail.com>
2025-01-13 09:44:32 +0530

    Scanner

--- day1.java
+++ day1.java
@@ -1,6 +1,8 @@
 // to run any program just perform this first
 // javac day1.java
 // java day1.java
+
+import java.util.Scanner;
 class day1 {
 
   // public static void main(String[] args) {
@@ -19,14 +21,27 @@ class day1 {
 
   // }
 
+  // public static void main(String[] args) {
+  //   float x = 6.9f;
+  //   float y = 3;
+  //   System.out.println("Learning arithmatics");
+  //   System.out.println(x+y); 
+  //   System.out.println(x-y); 
+  //   System.out.println(x*y); 
+  //   System.out.println(x/y);
+  // }
+
   public static void main(String[] args) {
-    float x = 6.9f;
-    float y = 3;
-    System.out.println("Learning arithmatics");
-    System.out.println(x+y); 
-    System.out.println(x-y); 
-    System.out.println(x*y); 
-    System.out.println(x/y);
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Printing the area using user input: ");
+    int x = sc.nextInt();
+
+    System.out.println("area of circle is: "+(x*3.14*x));
+
+    sc.close();
+
   }
 
 }
