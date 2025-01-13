51e152bf1c2d7084f1a51f9191c43719dbff8b95
Suryakant <surajdwivedi644@gmail.com>
2025-01-13 11:49:40 +0530

    learning

--- Conditionals/day1Cond.java
+++ Conditionals/day1Cond.java
@@ -1,15 +1,74 @@
 package Conditionals;
 import java.util.Scanner;
 public class day1Cond {
-  public static void main(String[] args) {
-    System.out.println("Even odd finder");
-    Scanner sc = new Scanner(System.in);
+  // public static void main(String[] args) {
+  //   System.out.println("Even odd finder");
+  //   Scanner sc = new Scanner(System.in);
+  //   int a = sc.nextInt();
+  //   if(a%2 == 0) System.out.println(a+" a even number");
+  //   else System.out.println(a+" a odd number");
+  //   sc.close();
+  // }
 
-    int a = sc.nextInt();
+  // public static void main(String[] args) {
+  //   System.out.println("Checking number is divisible by 5 or not");
+  //   Scanner sc = new Scanner(System.in);
+  //   int a = sc.nextInt();
+  //   if(a%5 == 0) System.out.println("divisible");
+  //   else System.out.println("not divisible");
+  //   sc.close();
+  // }
 
-    if(a%2 == 0) System.out.println(a+" a even number");
-    else System.out.println(a+" a odd number");
+  // public static void main(String[] args) {
+  //   System.out.println("Printing absolut value");
+  //   Scanner sc = new Scanner(System.in);
+  //     int a = sc.nextInt();
+  //     if(a < 0) a = -a;
+  //     System.out.println(a);
+  //     sc.close();
+  // }
 
-    sc.close();
-  }
+  // public static void main(String[] args) {
+  //   System.out.println("Profit loss calculator: ");
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.println("enter costPrice sellingPrice: ");
+  //   double cp = sc.nextDouble();
+  //   double sp = sc.nextDouble();
+
+  //   if(cp<sp) System.out.println("Made profit of: "+( sp-cp ));
+  //   else if(cp>sp) System.out.println("Made loss: "+(cp-sp));
+  //   else System.out.println("no profit no loss");
+
+  //   sc.close();
+  // }  
+
+  // public static void main(String[] args) {
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.println("Number of digits in input");
+  //   int a = sc.nextInt();
+  //   int b = a;
+  //   int cnt = 0;
+  //   while(b>0){
+  //      b = b / 10;
+  //     cnt++;
+  //   }
+  //   System.out.println("number of digits are: "+cnt);
+  //   sc.close();
+  // }
+
+  // public static void Triangle(int a, int b, int c){
+  //   if(a+b > c && a+c > b && b+c > a) System.out.println("Yes");
+  //   else System.out.println("no");
+  // }
+
+  // public static void main(String[] args) {
+  //   Scanner sc = new Scanner(System.in);
+  //   int a = sc.nextInt();
+  //   int b = sc.nextInt();
+  //   int c = sc.nextInt();
+  //   Triangle(a,b,c);
+
+  //   sc.close();
+  // }
+
 }
