3de56a348a220d72181a71f3092b27aa434bbe0b
Suryakant <surajdwivedi644@gmail.com>
2025-01-13 09:36:24 +0530

    climbing Up

--- day1.java
+++ day1.java
@@ -1,7 +1,32 @@
+// to run any program just perform this first
+// javac day1.java
+// java day1.java
 class day1 {
 
+  // public static void main(String[] args) {
+  //   System.out.println("hlo world");    
+  //   System.out.println("learning java day 1");
+  // }
+
+  // public static void main(String[] args) {
+  //   int x = 5;
+  //   int y = 2;
+  //   System.out.println("Learning arithmatics");
+  //   System.out.println(x+y); //7
+  //   System.out.println(x-y); //3
+  //   System.out.println(x*y); //10
+  //   System.out.println(x/y); //2
+
+  // }
+
   public static void main(String[] args) {
-    System.out.println("hlo world");
+    float x = 6.9f;
+    float y = 3;
+    System.out.println("Learning arithmatics");
+    System.out.println(x+y); 
+    System.out.println(x-y); 
+    System.out.println(x*y); 
+    System.out.println(x/y);
   }
 
 }
