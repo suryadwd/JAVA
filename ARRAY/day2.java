c19ca5e94ce81a229fbc6b9ddf180cfeba899f93
Suryakant <surajdwivedi644@gmail.com>
2025-01-17 19:49:52 +0530

    day5

--- ARRAY/day2.java
+++ ARRAY/day2.java
@@ -1,6 +1,6 @@
 package ARRAY;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class day2 {
 
--- ARRAY/day5.java
+++ ARRAY/day5.java
@@ -1,31 +1,79 @@
+package ARRAY;
+
 import java.util.Scanner;
 
 public class day5 {
 
-  public static int Celing(int[] nums, int target) {
+  // public static int Celing(int[] nums, int target) {
 
-    int low = 0, high = nums.length - 1;
+  //   int low = 0, high = nums.length - 1;
 
-    int ciling = 0;
+  //   int ciling = 0;
 
-    while (low <= high) {
+  //   while (low <= high) {
 
-      int mid = low + (high - low) / 2;
+  //     int mid = low + (high - low) / 2;
 
-      if (nums[mid] == target) {
+  //     if (nums[mid] == target) {
+  //       return nums[mid];
+  //     }
+
+  //     else if (nums[mid] < target) {
+  //       low = mid + 1;
+  //       ciling = low;
+  //     }
+
+  //     else
+  //       high = mid - 1;
+  //     ciling = low;
+  //   }
+
+  //   return nums[ciling];
+  // }
+
+  // public static void main(String[] args) {
+
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.print("Enter size: ");
+  //   int n = sc.nextInt();
+  //   System.out.println("Enter array elements");
+  //   int arr[] = new int[n];
+  //   for (int i = 0; i < n; i++) {
+  //     arr[i] = sc.nextInt();
+  //   }
+  //   System.out.println("Enter target: ");
+  //   int t = sc.nextInt();
+  //   System.out.println(Celing(arr, t));
+
+  //   sc.close();
+  // }
+
+  public static int Floar(int nums[], int target){
+
+    int low = 0, high = nums.length-1;
+
+    int Floar = 0;
+
+    while(low <= high){
+
+      int mid = low + (high - low)/2;
+
+      if(target == nums[mid]) {
         return nums[mid];
       }
 
-      else if (nums[mid] < target) {
+      else if(target > nums[mid]){
         low = mid + 1;
-        ciling = low;
+        Floar = low;
       }
 
-      else
+      else {
         high = mid - 1;
-      ciling = low;
+        Floar = low;
+      }
+
     }
-    return nums[ciling];
+    return nums[Floar-1];
   }
 
   public static void main(String[] args) {
@@ -40,7 +88,7 @@ public class day5 {
     }
     System.out.println("Enter target: ");
     int t = sc.nextInt();
-    System.out.println(Celing(arr, t));
+    System.out.println(Floar(arr, t));
 
     sc.close();
   }
