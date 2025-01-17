32df4d9deac727e2569b31b8116b62c68b5e36e6
Suryakant <surajdwivedi644@gmail.com>
2025-01-17 20:50:26 +0530

    day5

--- ARRAY/day5.java
+++ ARRAY/day5.java
@@ -48,32 +48,62 @@ public class day5 {
   //   sc.close();
   // }
 
-  public static int Floar(int nums[], int target){
+  // public static int Floar(int nums[], int target){
 
-    int low = 0, high = nums.length-1;
+  //   int low = 0, high = nums.length-1;
 
-    int Floar = 0;
+  //   int Floar = 0;
 
-    while(low <= high){
+  //   while(low <= high){
 
-      int mid = low + (high - low)/2;
+  //     int mid = low + (high - low)/2;
 
-      if(target == nums[mid]) {
-        return nums[mid];
-      }
+  //     if(target == nums[mid]) {
+  //       return nums[mid];
+  //     }
 
-      else if(target > nums[mid]){
-        low = mid + 1;
-        Floar = low;
-      }
+  //     else if(target > nums[mid]){
+  //       low = mid + 1;
+  //       Floar = low;
+  //     }
 
-      else {
-        high = mid - 1;
-        Floar = low;
-      }
+  //     else {
+  //       high = mid - 1;
+  //       Floar = low;
+  //     }
 
-    }
-    return nums[Floar-1];
+  //   }
+  //   return nums[Floar-1];
+  // }
+
+  // public static void main(String[] args) {
+
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.print("Enter size: ");
+  //   int n = sc.nextInt();
+  //   System.out.println("Enter array elements");
+  //   int arr[] = new int[n];
+  //   for (int i = 0; i < n; i++) {
+  //     arr[i] = sc.nextInt();
+  //   }
+  //   System.out.println("Enter target: ");
+  //   int t = sc.nextInt();
+  //   System.out.println(Floar(arr, t));
+
+  //   sc.close();
+  // }
+
+  public static void Occurance(int []arr, int target){
+
+
+    int i = 0;
+    int j = arr.length - 1;
+
+    while(arr[i] != target) i++;
+    while (arr[j]!= target) j--;
+    
+    System.out.println(i+" "+j);
+    
   }
 
   public static void main(String[] args) {
@@ -88,7 +118,7 @@ public class day5 {
     }
     System.out.println("Enter target: ");
     int t = sc.nextInt();
-    System.out.println(Floar(arr, t));
+    Occurance(arr, t);
 
     sc.close();
   }
