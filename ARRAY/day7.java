3c104c925c349275719b114f156c5187327dab2b
Suryakant <surajdwivedi644@gmail.com>
2025-01-19 13:22:22 +0530

    day7

--- ARRAY/day7.java
+++ ARRAY/day7.java
@@ -163,43 +163,139 @@ public class day7 {
 
   // 5 -> moving negative number to left side of an array
 
-  static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+  // static void swap(int[] arr, int i, int j) {
+  // int temp = arr[i];
+  // arr[i] = arr[j];
+  // arr[j] = temp;
+  // }
 
-  static void sider(int[] arr) {
-    int low = 0, high = arr.length - 1, itr = 0;
+  // static void sider(int[] arr) {
+  // int low = 0, high = arr.length - 1, itr = 0;
 
-    while (itr <= high) {
+  // while (itr <= high) {
 
-      if (arr[itr] < 0) {
-        swap(arr, low, itr);
-        low++;
-        itr++;
-      } else
-        itr++;
+  // if (arr[itr] < 0) {
+  // swap(arr, low, itr);
+  // low++;
+  // itr++;
+  // } else
+  // itr++;
+
+  // }
+
+  // for (int i = 0; i < arr.length; i++) {
+  // System.out.print(arr[i] + " ");
+  // }
+
+  // }
+
+  // public static void main(String[] args) {
+  // Scanner sc = new Scanner(System.in);
+  // System.out.print("Enter size: ");
+  // int n = sc.nextInt();
+  // System.out.println("Enter array elements");
+  // int arr[] = new int[n];
+  // for (int i = 0; i < n; i++) {
+  // arr[i] = sc.nextInt();
+  // }
+
+  // sider(arr);
+
+  // sc.close();
+  // }
+
+  // 6 -> Union and intersection of array
+
+  static void UniIntArray(int[] arr1, int[] arr2) {
+
+    int i = 0, j = 0, k = 0;
+
+    int uni[] = new int[arr1.length + arr2.length];
+
+    int intr[] = new int[Math.min(arr1.length, arr2.length)];
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        uni[k] = arr1[i];
+        i++;
+        k++;
+      } else if (arr1[i] > arr2[j]) {
+        uni[k] = arr2[j];
+        j++;
+        k++;
+      } else {
+        uni[k] = arr1[i];
+        i++;
+        j++;
+        k++;
+      }
 
     }
 
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
+    while (i < arr1.length) {
+      uni[k] = arr1[i];
+      k++;
+      i++;
+    }
+
+    while (j < arr2.length) {
+      uni[k] = arr2[j];
+      k++;
+      j++;
+    }
+    System.out.print("Union of the array: ");
+    for (int l : uni) {
+      System.out.print(l + " ");
+    }
+
+    i = 0;
+    j = 0;
+    k = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        i++;
+      } else if (arr1[i] > arr2[j]) {
+        j++;
+      } else {
+        intr[k] = arr1[i];
+        i++;
+        j++;
+        k++;
+      }
+
+    }
+
+    
+    System.out.println();
+
+    System.out.print("Intersection of Array: ");
+
+    for (int l : intr) {
+      System.out.print(l + " ");
     }
 
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter size: ");
+    System.out.print("Enter array 1 size: ");
     int n = sc.nextInt();
-    System.out.println("Enter array elements");
+    System.out.println("Enter array 1 elements");
     int arr[] = new int[n];
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
 
-    sider(arr);
+    System.out.print("Enter array 2 size: ");
+    int m = sc.nextInt();
+    System.out.println("Enter array 2 elements");
+    int brr[] = new int[m];
+    for (int i = 0; i < m; i++) {
+      brr[i] = sc.nextInt();
+    }
+
+    UniIntArray(arr, brr);
 
     sc.close();
   }
