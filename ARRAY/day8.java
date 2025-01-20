b22a1c5cf0163364a5809cc155561b6677297199
Suryakant <surajdwivedi644@gmail.com>
2025-01-20 15:49:11 +0530

    day8

--- ARRAY/day8.java
+++ ARRAY/day8.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class day8 {
 
-
   // 1->Finding the first Occurence of a number
 
   // static void firstOcc(int[] arr, int target) {
@@ -42,21 +41,21 @@ public class day8 {
   // }
 
   // static void lastOcc(int[] arr, int target) {
-  //   int low = 0, high = arr.length - 1;
-  //   int index = -1;
-    // while (low <= high) {
-    // int mid = low + (high - low) / 2;
+  // int low = 0, high = arr.length - 1;
+  // int index = -1;
+  // while (low <= high) {
+  // int mid = low + (high - low) / 2;
 
-    // if (arr[mid] == target) {
-    // index = mid;
-    // low = mid + 1;
-    // } else if (arr[mid] > target)
-    // high = mid - 1;
-    // else
-    // low = mid + 1;
+  // if (arr[mid] == target) {
+  // index = mid;
+  // low = mid + 1;
+  // } else if (arr[mid] > target)
+  // high = mid - 1;
+  // else
+  // low = mid + 1;
 
-    // }
-    // System.out.println("Last occ: " + index);
+  // }
+  // System.out.println("Last occ: " + index);
   // }
 
   // public static void main(String[] args) {
@@ -116,50 +115,85 @@ public class day8 {
   // sc.close();
   // }
 
-  static void AddMatrix(int [][]arr, int brr[][],int n, int m){
-    int crr [][] = new int[n][m];
+  // static void AddMatrix(int[][] arr, int brr[][], int n, int m) {
+  //   int crr[][] = new int[n][m];
 
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < m; j++) {
+  //       crr[i][j] = arr[i][j] + brr[i][j];
+  //     }
+  //   }
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < m; j++) {
+  //       System.out.print(crr[i][j] + " ");
+  //     }
+  //     System.out.println();
+  //   }
+
+  // }
+
+  // public static void main(String[] args) {
+
+  //   Scanner sc = new Scanner(System.in);
+  //   System.out.print("Enter rows and col: ");
+  //   int n = sc.nextInt();
+  //   int m = sc.nextInt();
+
+  //   int arr[][] = new int[n][m];
+  //   int brr[][] = new int[n][m];
+  //   System.out.println("First array");
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < m; j++) {
+  //       arr[i][j] = sc.nextInt();
+  //     }
+  //   }
+  //   System.out.println("Second array");
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < m; j++) {
+  //       brr[i][j] = sc.nextInt();
+  //     }
+  //   }
+
+  //   AddMatrix(arr, brr, n, m);
+
+  //   sc.close();
+  // }
+
+  static void Transpose(int [][] arr,int n, int m){
+    System.out.println("Your array: ");
     for(int i = 0; i < n; i++){
-      for(int j = 0; j < m; j++){
-        crr[i][j] = arr[i][j]+brr[i][j];
+      for(int j= 0; j < m; j++){
+        System.out.print(arr[i][j]+" ");
       }
+      System.out.println();
     }
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < m; j++){
-        System.out.print(crr[i][j]+" ");
+    System.out.println("Transposed matrix: ");
+    for(int i = 0; i < m; i++){
+      for(int j= 0; j < n; j++){
+        System.out.print(arr[j][i]+" ");
       }
       System.out.println();
     }
 
-    
-
   }
 
   public static void main(String[] args) {
-    
+
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter rows and col: ");
     int n = sc.nextInt();
     int m = sc.nextInt();
 
     int arr[][] = new int[n][m];
-    int brr[][] = new int[n][m];
     System.out.println("First array");
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < m;j++){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
         arr[i][j] = sc.nextInt();
       }
     }
-    System.out.println("Second array");
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < m;j++){
-        brr[i][j] = sc.nextInt();
-      }
-    }
-
-
-    AddMatrix(arr,brr,n,m);
+    Transpose(arr,n,m);
 
     sc.close();
   }
+
 }
