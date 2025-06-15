03d4538181ee25db543d9fd393e75ed05f28cd4f
Suryakant <surajdwivedi644@gmail.com>
2025-06-15 10:40:49 +0530

    day 1 second

--- Dailyjava/Main.java
+++ Dailyjava/Main.java
@@ -39,6 +39,24 @@ class SLL{
       System.out.print(temp.data+" ");
       temp = temp.next;
     }
+    System.out.println();
+  }
+
+  void printRec(Node head){
+    Node temp = head;
+    if(temp == null) return;
+    System.out.print(temp.data+" ");
+    printRec(temp.next);
+  }
+
+  void insertAtHead(int value){
+    Node temp = new Node(value);
+    if(head == null) head = tail = temp;
+    else{
+      temp.next = head;
+      head = temp;
+    }
+    size++;
   }
 
 }
@@ -53,5 +71,10 @@ public class Main {
       ll.insertAtEnd(4);
       ll.insertAtEnd(5);
       ll.printN();
+      ll.insertAtHead(9);
+      ll.insertAtHead(8);
+      ll.insertAtHead(7);
+      ll.printN();
+      
     }
 }
