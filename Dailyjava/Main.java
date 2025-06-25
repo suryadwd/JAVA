98a1e7844ee239ddc78b705bee4522ccf9dcb6d6
Suryakant <surajdwivedi644@gmail.com>
2025-06-25 14:47:35 +0530

    added up

--- Dailyjava/Main.java
+++ Dailyjava/Main.java
@@ -380,3 +380,26 @@ class Solution {
         return binarySearch(arr,k);
     }
 }
+
+class Solution {
+
+    public static void sort(int arr[]){
+        for(int i = 0;  i< arr.length; i++){
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+       sort(nums);
+       for(int i = 1; i < nums.length; i++){
+        if(nums[i - 1] == nums[i]) return true;
+       }
+       return false;
+    }
+}
