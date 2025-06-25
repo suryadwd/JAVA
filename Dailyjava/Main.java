05586da0e4e48b9aad6db93a8bc9592ca5d05394
Suryakant <surajdwivedi644@gmail.com>
2025-06-25 14:46:33 +0530

    added up

--- Dailyjava/Main.java
+++ Dailyjava/Main.java
@@ -346,3 +346,17 @@ public class Solution {
 
     }
 }
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int x : arr) map.put(x , map.getOrDefault(x, 0) + 1);
+        
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int x: map.values()) set.add(x);
+
+        return set.size() == map.size();
+    }
+}
