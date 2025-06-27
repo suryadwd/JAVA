eb03fb3d4e8d00d1dadbca387db331bca302be6d
Suryakant <surajdwivedi644@gmail.com>
2025-06-27 11:42:55 +0530

    added

--- Dailyjava/Main.java
+++ Dailyjava/Main.java
@@ -432,3 +432,25 @@ class Solution {
         return maxDpt;
     }
 }
+
+import java.util.*;
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack= new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == c){
+             stack.pop();
+             // stack me jo upar wala element wo nikalo aur same hue to delete kro
+            }else{
+                stack.push(c);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for(char c : stack){
+            res.append(c);
+        }
+        return res.toString();
+    }
+}
