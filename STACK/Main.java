e2be79bd908d182bcd4a0028e7b91debd16f4ced
Suryakant <surajdwivedi644@gmail.com>
2025-06-27 22:39:40 +0530

    added

---
+++ STACK/Main.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+// class Main{
+//   public static void main(String[] args) {
+//     Stack<Integer> s = new Stack<>();
+//     s.push(1);
+//     s.push(2);
+//     s.push(3);
+//     System.out.println(s.peek());
+//     System.out.println(s.pop());
+//     System.out.println(s.peek());
+//   }
+// }
+
