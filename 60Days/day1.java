34c09fea41499b053b5daad8620811faa97f8d97
suryadwd <surajdwivedi644@gmail.com>
2025-07-14 14:29:34 +0530

    day 1 goal

--- 60Days/day1.java
+++ 60Days/day1.java
@@ -156,6 +156,21 @@ class Solution {
 }
 
 
+145. Binary Tree Postorder Traversal
 
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+    }
+    public void helper(TreeNode root, List<Integer> list){
+        if(root == null) return;
+        helper(root.left, list);
+        helper(root.right, list);
+        list.add(root.val);
+    }
+}
 
 
