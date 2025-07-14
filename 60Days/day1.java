5bb7670e3a3b57ca40e2b40ea47ea6a020e4a8d2
suryadwd <surajdwivedi644@gmail.com>
2025-07-14 14:30:04 +0530

    day 1 goal

--- 60Days/day1.java
+++ 60Days/day1.java
@@ -173,4 +173,18 @@ class Solution {
     }
 }
 
+144. Binary Tree Preorder Traversal
 
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+    }
+    public void helper(TreeNode root, List<Integer> list){
+        if(root == null) return;
+        list.add(root.val);
+        helper(root.left, list);
+        helper(root.right, list);
+    }
+}
