506180cf1d0f88abf5e72d6d137c975c4f341cca
suryadwd <surajdwivedi644@gmail.com>
2025-07-14 14:28:54 +0530

    day 1 goal

--- 60Days/day1.java
+++ 60Days/day1.java
@@ -129,3 +129,33 @@ class Solution {
         return result;
     }
 }
+
+102. Binary Tree Level Order Traversal
+
+
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return result;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+             result.add(list);
+        }
+        return result;
+    }
+}
+
+
+
+
+
