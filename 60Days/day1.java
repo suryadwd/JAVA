0b856cb598b3df90ff216463feff8d164a8212cc
suryadwd <surajdwivedi644@gmail.com>
2025-07-14 14:28:10 +0530

    day 1 goal

--- 60Days/day1.java
+++ 60Days/day1.java
@@ -97,3 +97,35 @@ class Solution {
     }
 }
 
+103. Binary Tree Zigzag Level Order Traversal
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true; 
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> list = new LinkedList<>(); 
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) list.addLast(node.val); 
+                else list.addFirst(node.val);            
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            result.add(list);
+            leftToRight = !leftToRight; 
+        }
+
+        return result;
+    }
+}
