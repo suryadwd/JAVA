df9a497fbb4a0fb16d3772548c5c84f902d544ce
suryadwd <surajdwivedi644@gmail.com>
2025-07-17 09:40:47 +0530

    day 4 of goals

--- 60Days/day1.java
+++ 60Days/day1.java
@@ -381,3 +381,14 @@ class Solution {
         return list;
     }
 }
+
+700. Search in a Binary Search Tree
+
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val) return root;
+        else if(val < root.val) return searchBST(root.left, val);
+        else return  searchBST(root.right, val);
+    }
+}
