6f080a108ff248502286ff67fd697a48fb1f70e9
Suryakant <surajdwivedi644@gmail.com>
2025-08-03 23:16:34 +0530

    aim 1 - palindrome checker

--- August/Main.java
+++ August/Main.java
@@ -7,7 +7,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    System.out.println(digitReverse(a));
+    System.out.println(checkPalindrome(a));
     sc.close();
   }
 
@@ -30,5 +30,16 @@ public class Main {
     return digitReverse(n / 10);
   }
 
+
+  public static boolean checkPalindrome(int n) {
+    int check = n;
+    int rev = 0;
+    while (check != 0) {
+      int digit = check%10;
+      rev = rev * 10 + digit;
+      check = check/10;
+    }
+    return rev == n;
+  }
 
 }
