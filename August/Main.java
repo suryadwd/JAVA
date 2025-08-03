fb715489c108b901bd2c0ab9bc8a6f79e72e28ac
Suryakant <surajdwivedi644@gmail.com>
2025-08-03 22:38:37 +0530

    aim 1 - digit multiply

--- August/Main.java
+++ August/Main.java
@@ -4,7 +4,8 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
-    System.out.println(digitSum(a));
+    System.out.println("Sum: "+digitSum(a));
+    System.out.println("Multiply: "+digitMul(a));
     sc.close();
   }
 
@@ -12,7 +13,12 @@ public class Main {
     if(n == 0) return 0;
     int digit = n % 10;
     return digit + digitSum(n/10);
-    
+  }
+
+  public static int digitMul(int n){
+   if(n == 0) return 1;
+   int digit = n % 10;
+   return digit * digitMul(n/10); 
   }
 
 }
