443af7d77103871d9b01232418227f65ff9909a3
suryadwd <surajdwivedi644@gmail.com>
2025-08-07 21:02:59 +0530

    day 2

--- August/Main.java
+++ August/Main.java
@@ -161,3 +161,22 @@ class Solution {
     }
 }
 }
+
+
+
+import java.util.*;
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		print(n);
+	}
+	public static void print(int n) {
+		if(n == 0) return;
+		print(n-1);
+		for(int i = 0; i < n; i++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+}
